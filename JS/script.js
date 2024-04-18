fa4ae5c02c8b84d25cf872304f31b5d4c12756a0
Eslam-Hussien <eslam01003733553@gmail.com>
2024-04-18 09:32:40 +0200

    add files

---
+++ JS/script.js
@@ -0,0 +1,70 @@
+
+$(document).ready(function(){
+    $('.floatingButton').on('click',
+        function(e){
+            e.preventDefault();
+            $(this).toggleClass('open');
+            if($(this).children('.fa').hasClass('fa-plus'))
+            {
+                $(this).children('.fa').removeClass('fa-plus');
+                $(this).children('.fa').addClass('fa-close');
+            } 
+            else if ($(this).children('.fa').hasClass('fa-close')) 
+            {
+                $(this).children('.fa').removeClass('fa-close');
+                $(this).children('.fa').addClass('fa-plus');
+            }
+            $('.floatingMenu').stop().slideToggle();
+        }
+    );
+    $(this).on('click', function(e) {
+      
+        var container = $(".floatingButton");
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && $('.floatingButtonWrap').has(e.target).length === 0) 
+        {
+            if(container.hasClass('open'))
+            {
+                container.removeClass('open');
+            }
+            if (container.children('.fa').hasClass('fa-close')) 
+            {
+                container.children('.fa').removeClass('fa-close');
+                container.children('.fa').addClass('fa-plus');
+            }
+            $('.floatingMenu').hide();
+        }
+      
+        // if the target of the click isn't the container and a descendant of the menu
+        if(!container.is(e.target) && ($('.floatingMenu').has(e.target).length > 0)) 
+        {
+            $('.floatingButton').removeClass('open');
+            $('.floatingMenu').stop().slideToggle();
+        } 
+    });
+});
+
+
+//      Typed js
+
+const typed = new Typed('.multiple-text' , {
+    strings: ['نقل عفش' , 'فك و تركيب الاثاث' , 'تغليف الاثاث' , 'بيع الكراتين'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+})
+
+$(document).ready(function() {
+    $(function(){
+        $('#menu-icon').click(function(){
+            $('.navbar-phone').slideToggle(500);
+        })
+    })
+    $(function(){
+        $('.nav-item').click(function(){
+            $('.navbar-phone').slideUp(500);
+        })
+    })
+  });
